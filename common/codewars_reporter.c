2be104d23937469503fdaec39373cf6c492389ee
Donald Sebastian Leung <donaldsebleung@gmail.com>
2022-07-30 11:55:53 +0800

    Remove redundant outermost suite; prefix commands with newline

--- common/codewars_reporter.c
+++ common/codewars_reporter.c
@@ -3,33 +3,33 @@
 #include <stdio.h>
 
 static void codewars_reporter_start_suite(TestReporter *reporter, const char *name, int count) {
-  printf("<DESCRIBE::>%s\n", name);
+  printf("\n<DESCRIBE::>%s\n", name);
   reporter_start_suite(reporter, name, count);
 }
 
 static void codewars_reporter_start_test(TestReporter *reporter, const char *name) {
-  printf("<IT::>%s\n", name);
+  printf("\n<IT::>%s\n", name);
   reporter_start_test(reporter, name);
 }
 
 static void codewars_show_pass(TestReporter *reporter, const char *file, int line, const char *message, va_list arguments) {
-  printf("<PASSED::>Test Passed\n");
+  printf("\n<PASSED::>Test Passed\n");
 }
 
 static void codewars_show_fail(TestReporter *reporter, const char *file, int line, const char *message, va_list arguments) {
-  printf("<FAILED::>");
+  printf("\n<FAILED::>");
   vprintf(message, arguments);
   printf("\n");
 }
 
 static void codewars_reporter_finish_test(TestReporter *reporter, const char *filename, int line, const char *message) {
   reporter_finish_test(reporter, filename, line, message);
-  printf("<COMPLETEDIN::>\n");
+  printf("\n<COMPLETEDIN::>\n");
 }
 
 static void codewars_reporter_finish_suite(TestReporter *reporter, const char *filename, int line) {
   reporter_finish_suite(reporter, filename, line);
-  printf("<COMPLETEDIN::>\n");
+  printf("\n<COMPLETEDIN::>\n");
 }
 
 TestReporter *create_codewars_reporter() {
--- common/tests.c
+++ common/tests.c
@@ -4,7 +4,5 @@ TestSuite *solution_tests();
 TestReporter *create_codewars_reporter();
 
 int main(int argc, char **argv) {
-  TestSuite *suite = create_test_suite();
-  add_suite(suite, solution_tests());
-  return run_test_suite(suite, create_codewars_reporter());
+  return run_test_suite(solution_tests(), create_codewars_reporter());
 }
